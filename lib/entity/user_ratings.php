32574e27a7fa23611a8d648597a57eeef075167b
Tai-Jin Lee <taijin@gmail.com>
2011-11-13 23:17:10 -0800

    removing skeleton

--- lib/entity/user_ratings.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace lib\entity;
-
-class user_ratings extends \lib\entity {
-
-  protected static $database = 'wine';
-  protected static $table = 'user_ratings';
-
-}
--- lib/entity/wine.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace lib\entity;
-
-class wine extends \lib\entity {
-
-  protected static $database = 'wine';
-  protected static $table = 'wines';
-
-}
